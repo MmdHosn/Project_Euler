e2c8bbb6577cecde12eba1ce13c794cea0700495
Mmd_Dev <grandplantracking@gmail.com>
2025-11-07 21:57:07 +0330

    Problem 1: 5/5 Test case complete

--- Problem 1.cpp
+++ Problem 1.cpp
@@ -2,28 +2,29 @@
 
 using namespace std;
 
-double Sum_Upto_N(int Number, int Mult_Of_X);
+long int Sum_Upto_N(int Number, int Mult_Of_X);
 
 int main()
 {
     int T;
     cin >> T;
+    cout << "Output:" << endl;
     while (T--)
     {
         int N;
         cin >> N;
-        double Final_Sum = Sum_Upto_N(N-1,3)+Sum_Upto_N(N-1,5)-Sum_Upto_N(N-1,15);
-        cout << Final_Sum <<endl;
+        long int Final_Sum = Sum_Upto_N(N - 1, 3) + Sum_Upto_N(N - 1, 5) - Sum_Upto_N(N - 1, 15);
+        cout << Final_Sum << endl;
     }
     return 0;
 }
 
-
-double Sum_Upto_N(int Number, int Mult_Of_X) {
-    double Sum = 0;
-    int N=Number;
+long int Sum_Upto_N(int Number, int Mult_Of_X)
+{
+    long int Sum = 0;
+    int N = Number;
     int X = Mult_Of_X;
-    int Num_Count = (N/X);
-    Sum = X*(Num_Count*(Num_Count+1))/2;
+    long int Num_Count = (N / X);
+    Sum = X * (Num_Count * (Num_Count + 1)) / 2;
     return Sum;
 }
