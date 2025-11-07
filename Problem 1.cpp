63b022127844851bd61fd211a37b14b135bcd367
Mmd_Dev <grandplantracking@gmail.com>
2025-11-07 21:11:27 +0330

    Problem 1 Complete, but doesn't work on 2 out of 5 test cases.

--- Problem 1.cpp
+++ Problem 1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 using namespace std;
-int Sum_Upto_N(int Number,int Mult_Of_X);
+
+int Sum_Upto_N(int Number, int Mult_Of_X);
 
 int main()
 {
@@ -12,18 +13,18 @@ int main()
     {
         int N;
         cin >> N;
-        cout << Sum_Upto_N(N,3)<<endl;
+        int Final_Sum = Sum_Upto_N(N - 1, 3) + Sum_Upto_N(N - 1, 5) - Sum_Upto_N(N - 1, 15);
+        cout << Final_Sum << endl;
     }
     return 0;
 }
 
-
-
-int Sum_Upto_N(int Number,int Mult_Of_X) {
+int Sum_Upto_N(int Number, int Mult_Of_X)
+{
     int Sum = 0;
-    int N=Number;
+    int N = Number;
     int X = Mult_Of_X;
-    int Num_Count = (N/X);
-    Sum = X*(Num_Count*(Num_Count+1))/2;
+    int Num_Count = (N / X);
+    Sum = X * (Num_Count * (Num_Count + 1)) / 2;
     return Sum;
 }
