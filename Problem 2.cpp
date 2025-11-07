0b09451b413c7658bbc3d5f16200beb2d914b8a2
Mmd_Dev <grandplantracking@gmail.com>
2025-11-07 23:12:46 +0330

    Problem 2 Completed:2/5 Test Cases Failed

--- Problem 2.cpp
+++ Problem 2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+long long Even_Fib_Adder(long long Upto_Fib_Num_Value);
 
 int main()
 {
@@ -11,7 +12,24 @@ int main()
     {
         int N;
         cin >> N;
-        cout << N << endl;
+        cout << Even_Fib_Adder(N) << endl;
     }
     return 0;
+}
+
+
+
+long long Even_Fib_Adder(long long Upto_Fib_Num_Value) {
+    long long Even_Fib_Sum = 0;
+    long long Previous_Fib_Number = 0;
+    long long Current_Fib_Number = 1;
+    while(Current_Fib_Number <= Upto_Fib_Num_Value) {
+        long long tmp = Current_Fib_Number;
+        Current_Fib_Number += Previous_Fib_Number;
+        Previous_Fib_Number = tmp;
+        if (Previous_Fib_Number % 2 == 0) {
+            Even_Fib_Sum += Previous_Fib_Number;
+        }
+    }
+    return Even_Fib_Sum;
 }
