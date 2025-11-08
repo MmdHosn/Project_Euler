26048a2d1be196a62485eb80d502349cca820b2a
Mmd_Dev <grandplantracking@gmail.com>
2025-11-09 02:27:59 +0330

    Added a function to check whether a Number is 6-digit palindrome

--- Problem 4.cpp
+++ Problem 4.cpp
@@ -2,16 +2,42 @@
 
 using namespace std;
 
+// checks whether Input is a 6-Digit Palindrome Number
+bool Is_6Digit_Palindrome(int Number);
+// checks whether Input is Multiply of two 3-Digit Numbers
+bool Is_Mult_Two_3Digit(int Number);
+// Finds biggest Palindrome Number that is multiple of two 3-Digit numbers
+int Biggest_Palindrome(int Number);
+
 int main()
 {
     int T;
     cin >> T;
-    cout << "Output:" << endl;
+    cout << " Output:" << endl;
     while (T--)
     {
         int N;
         cin >> N;
-        cout << N << endl;
+        cout << (Is_6Digit_Palindrome(N) ? "true" : "false") << endl;
     }
     return 0;
+}
+
+bool Is_6Digit_Palindrome(int Number)
+{
+    if (99999 < Number && Number < 1000000)
+    {
+        int First_3Digits_Inverse = 0;
+        int Second_3Digits = Number;
+        for (int i = 0; i < 3; i++)
+        {
+            int tmp = Second_3Digits % 10;
+            Second_3Digits /= 10;
+            First_3Digits_Inverse *= 10;
+            First_3Digits_Inverse += tmp;
+        }
+        if (First_3Digits_Inverse == Second_3Digits)
+            return true;
+    }
+    return false;
 }
