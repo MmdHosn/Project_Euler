15334ea398066e737d1617326a430d4088afe624
Mmd_Dev <grandplantracking@gmail.com>
2025-11-13 16:52:41 +0330

    Problem 5: added sieve of Erathosthenes as a function

--- Problem 5.cpp
+++ Problem 5.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void Sieve_Erathosthenes(vector<int> &prime, int Max);
 
 int main()
 {
     int T;
     cin >> T;
     cout << "Output:" << endl;
+    vector<int> prime;
+    Sieve_Erathosthenes(prime, 300);
+    for (auto i : prime)
+    {
+        cout << i << endl;
+    }
     while (T--)
     {
         int N;
@@ -14,4 +23,29 @@ int main()
         cout << N << endl;
     }
     return 0;
+}
+
+void Sieve_Erathosthenes(vector<int> &prime, int Max)
+{
+    vector<bool> isPrime(Max + 1, true);
+
+    for (int i = 2; i * i <= Max; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= Max; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+    for (int i = 2; i <= Max; i++)
+    {
+
+        if (isPrime[i])
+        {
+
+            prime.push_back(i);
+        }
+    }
 }
