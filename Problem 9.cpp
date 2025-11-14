c346ec47786fc11146619938045e1093e9a97f36
MmdHosn <grandplantracking@gmail.com>
2025-11-15 00:20:27 +0330

    Problem 9 complete

--- Problem 9.cpp
+++ Problem 9.cpp
@@ -13,14 +13,20 @@ int main()
     {
         int N;
         cin >> N;
-        cout << N << endl;
+        cout << pythagorean_triplet_Max(N) << endl;
     }
     return 0;
+}
+bool is_pythagorean(int a, int b, int c)
+{
+    if (a * a + b * b == c * c)
+        return true;
+    return false;
 }
 
 int pythagorean_triplet_Max(int Number)
 {
-    int Max = 0;
+    int Max = -1;
     for (int i = 3; i <= Number / 3; i++)
     {
         int j = i + 1, k = Number - i - j;
@@ -28,8 +34,9 @@ int pythagorean_triplet_Max(int Number)
         {
             if (is_pythagorean(i, j, k))
             {
-                Max = (Max<i*j*k)? i * j * k : Max;
+                Max = (Max < i * j * k) ? i * j * k : Max;
             }
+            j++;
         }
     }
     return Max;
