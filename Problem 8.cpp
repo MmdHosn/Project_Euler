86bd8bdee1007175627d6f9e5ff414f68b6317eb
MmdHosn <grandplantracking@gmail.com>
2025-11-14 22:46:57 +0330

    Problem 8 Complete

--- Problem 8.cpp
+++ Problem 8.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
 
 using namespace std;
+int Largest_Mult_inSeries(int Serie_Size, int Multi_Size, const string &Serie);
 
 int main()
 {
     int T;
     cin >> T;
-    cout << "Output:" << endl;
     while (T--)
     {
-        int N;
-        cin >> N;
-        cout << N << endl;
+        int Serie_Size, Multi_Size;
+        string Serie;
+        cin >> Serie_Size >> Multi_Size >> Serie;
+
+        cout << Largest_Mult_inSeries(Serie_Size, Multi_Size, Serie) << endl;
     }
     return 0;
+}
+
+int Multiplier(int Multi_Size, string Serie)
+{
+    int Mult = 1;
+    for (int i = 0; i < Multi_Size; i++)
+    {
+        Mult *= (Serie.back() - '0');
+        Serie.pop_back();
+    }
+    return Mult;
+}
+
+int Largest_Mult_inSeries(int Serie_Size, int Multi_Size, const string &Serie)
+{
+    int Max = 0;
+    for (int i = 0; i < Serie_Size - Multi_Size + 1; i++)
+    {
+        int tmp = Multiplier(Multi_Size, Serie.substr(i, Multi_Size));
+        if (Max < tmp)
+        {
+            Max = tmp;
+        }
+    }
+    return Max;
 }
