71fa181be65429866bd6f5fd3b8072ed983a1a14
MmdHosn <grandplantracking@gmail.com>
2025-11-24 04:55:22 +0330

    Problem 10 initial structure added

---
+++ Problem 10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    cout << "Output:" << endl;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << N << endl;
+    }
+    return 0;
+}
